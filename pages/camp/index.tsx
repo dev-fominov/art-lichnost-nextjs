eed8956a00152ae7c53317fb107a82ca668fd5e9
dev-fominov <dev-fominov@yandex.by>
2022-08-14 13:18:43 +0300

    create app

---
+++ pages/camp/index.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Index: NextPage = () => {
+  return (
+    <div>Hello Camp</div>
+  )
+}
+
+export default Index
---
+++ pages/contacts.tsx
@@ -0,0 +1,9 @@
+import type { NextPage } from 'next'
+
+const Contacts: NextPage = () => {
+  return (
+    <div>Hello Contacts</div>
+  )
+}
+
+export default Contacts

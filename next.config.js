6133975fdb73f662c897b9972550fd741b463360
dev-fominov <dev-fominov@yandex.by>
2022-09-26 08:35:23 +0300

    merge mainPage to main

--- next.config.js
+++ next.config.js
@@ -1,23 +1,23 @@
 // Для выгрузки в сеть
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: false,
-//   swcMinify: true,
-//   images: {
-//     loader: "akamai",
-//     path: "",
-//   },
-//   basePath: "/art-lichnost-nextjs",
-//   assetPrefix: "/art-lichnost-nextjs",
-// }
-
-// module.exports = nextConfig
-
-// для локальной работы
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
   swcMinify: true,
+  images: {
+    loader: "akamai",
+    path: "",
+  },
+  basePath: "/art-lichnost-nextjs",
+  assetPrefix: "/art-lichnost-nextjs",
 }
 
 module.exports = nextConfig
+
+// для локальной работы
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   swcMinify: true,
+// }
+
+// module.exports = nextConfig

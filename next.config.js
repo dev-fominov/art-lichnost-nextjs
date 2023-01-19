306fa51e790f443199469b41b33687cc0027ccd5
dev-fominov <dev-fominov@yandex.by>
2023-01-19 16:52:46 +0300

    fix next config

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   swcMinify: true,
   crossOrigin: 'anonymous',
   images: {
-    domains: ['alex-volkov.ru'],
+    domains: ['api.art-lichnost.ru'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },

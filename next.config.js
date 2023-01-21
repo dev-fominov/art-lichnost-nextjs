6e89af1a8b10b9d4c8b8803e8b65bea7718b9155
dev-fominov <dev-fominov@yandex.by>
2023-01-21 07:28:30 +0300

    add 404 page

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   swcMinify: true,
   crossOrigin: 'anonymous',
   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'api.art-lichnost.ru',
+        port: '',
+        pathname: '/*',
+      },
+    ],
     domains: ['api.art-lichnost.ru'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],

a8c8864adf883172c3089a0c8dfa4ef135159346
dev-fominov <dev-fominov@yandex.by>
2023-01-22 06:48:25 +0300

    add sitemap.xml

--- components/common/Page404.tsx
+++ components/common/Page404.tsx
@@ -1,18 +1,18 @@
-import {Container} from "./Container";
 import {A} from "./A";
 import styles from '../../styles/Home.module.css'
+import { Section } from "./Section";
 
 const Page404 = () => {
     return (
         <>
-            <Container>
+            <Section>
                 <div className={styles.contentBox}>
                     <h2>Возможно, она была перемещена, или вы неверно указали адрес страницы.</h2>
                     <div className={styles.lickBox}>
                         <A href={'/'} text={'Перейти на главную'} className={styles.lick}/>
                     </div>
                 </div>
-            </Container>
+            </Section>
         </>
     )
 }

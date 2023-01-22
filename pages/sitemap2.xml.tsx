21a29ebb966c52f534559692e90138335b10365d
dev-fominov <dev-fominov@yandex.by>
2023-01-22 09:33:31 +0300

    fix multiple sitemap

--- pages/sitemap2.xml.tsx
+++ pages/sitemap2.xml.tsx
@@ -8,6 +8,23 @@ const Sitemap2 = () => {
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 
+	const staticPaths = [
+		'https://art-lichnost.ru/about',
+		'https://art-lichnost.ru/blog',
+		'https://art-lichnost.ru/camp',
+		'https://art-lichnost.ru/contacts',
+		'https://art-lichnost.ru/contract',
+		'https://art-lichnost.ru/courses',
+		'https://art-lichnost.ru/documents',
+		'https://art-lichnost.ru',
+		'https://art-lichnost.ru/merch',
+		'https://art-lichnost.ru/proftestirovanie',
+		'https://art-lichnost.ru/projects',
+		'https://art-lichnost.ru/psychologist',
+		'https://art-lichnost.ru/team',
+		'https://art-lichnost.ru/vacancies',
+	]
+
 	const allPaths = [...staticPaths]
 	const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
@@ -30,22 +47,21 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
 	}
 }
 
-const staticPaths = fs
-	.readdirSync("pages")
-	.filter(staticPage => {
-		return ![
-			"api",
-			"_app.tsx",
-			"404.tsx",
-			"_document.js",
-			"sitemap.xml.tsx",
-			"sitemap1.xml.tsx",
-			"sitemap2.xml.tsx",
-			"sitemap3.xml.tsx",
-		].includes(staticPage)
-	})
-	.map(staticPagePath => {
-		return `${baseURLSite}/${staticPagePath.split('.')[0]}`
-	})
+// const staticPaths = fs
+// 	.readdirSync("pages")
+// 	.filter(staticPage => {
+// 		return ![
+// 			"api",
+// 			"_app.tsx",
+// 			"404.tsx",
+// 			"sitemap.xml.tsx",
+// 			"sitemap1.xml.tsx",
+// 			"sitemap2.xml.tsx",
+// 			"sitemap3.xml.tsx",
+// 		].includes(staticPage)
+// 	})
+// 	.map(staticPagePath => {
+// 		return `${baseURLSite}/${staticPagePath.split('.')[0]}`
+// 	})
 
 export default Sitemap2

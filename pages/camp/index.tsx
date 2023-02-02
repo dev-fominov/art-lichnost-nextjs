c367f6c716d18b81dbcd65577cb0efb2237f5986
dev-fominov <dev-fominov@yandex.by>
2023-02-02 11:16:06 +0300

    fix camp video art-lichnost

--- pages/camp/index.tsx
+++ pages/camp/index.tsx
@@ -44,7 +44,7 @@ const Camp: NextPage = ({ data }: any) => {
 						height={VIDEO_HEIGHT}
 						frameBorder="0"
 						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-						src={`https://www.youtube.com/embed/${data.background_video}?autoplay=1&controls=0&rel=0&showinfo=0&mute=1&modestbranding=1&iv_load_policy=3&playsinline=1&enablejsapi=1&origin=https%3A%2F%2Fart-lichnost.vercel.app&widgetid=1`}
+						src={`https://www.youtube.com/embed/${data.background_video}?autoplay=1&controls=0&rel=0&showinfo=0&mute=1&modestbranding=1&iv_load_policy=3&playsinline=1&enablejsapi=1&origin=https%3A%2F%2Fart-lichnost.ru&widgetid=1`}
 					/>
 					<ContentHeader />
 				</div>

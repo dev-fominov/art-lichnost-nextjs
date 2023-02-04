757375d6fe5144d12ca1973ba81337cd267f1496
dev-fominov <dev-fominov@yandex.by>
2023-02-04 12:50:32 +0300

    fix

--- pages/blog/[post]/index.tsx
+++ pages/blog/[post]/index.tsx
@@ -1,43 +1,43 @@
-import {HeaderGreen} from "../../../components/common/HeaderGreen";
-import {Section} from "../../../components/common/Section";
+import { HeaderGreen } from "../../../components/common/HeaderGreen";
+import { Section } from "../../../components/common/Section";
 import styles from '../../../styles/blog/post.module.css'
-import {Footer} from "../../../components/common/Footer";
-import {NextPage} from "next";
+import { Footer } from "../../../components/common/Footer";
+import { NextPage } from "next";
 import Meta from "../../../services/Meta";
-import {pageAPI} from "../../../api/api";
+import { pageAPI } from "../../../api/api";
 
-const Post: NextPage = ({data}: any) => {
-    return (
-        <>
-            <Meta meta={data.metadata}/>
-            <HeaderGreen title={data.title}/>
-            <Section>
-                <div>
-                    <h3 className={styles.title}>{data.title}</h3>
-                    <div className={styles.content} dangerouslySetInnerHTML={{__html: data.content}}/>
-                </div>
-            </Section>
-            <Footer/>
-        </>
-    )
+const Post: NextPage = ({ data }: any) => {
+	return (
+		<>
+			<Meta meta={data.metadata} />
+			<HeaderGreen title={data.title} />
+			<Section>
+				<div>
+					<h3 className={styles.title}>{data.title}</h3>
+					<div className={styles.content} dangerouslySetInnerHTML={{ __html: data.content }} />
+				</div>
+			</Section>
+			<Footer />
+		</>
+	)
 }
 
 export default Post
 
 export async function getStaticPaths() {
-    const data =  await pageAPI.blogs()
-    const paths = data.posts.map((item: any) => ({
-        params: { post: item.slug },
-    }))
-    return { paths, fallback: false }
+	const data = await pageAPI.blogs()
+	const paths = data.posts.map((item: any) => ({
+		params: { post: item.slug },
+	}))
+	return { paths, fallback: false }
 }
 
 export async function getStaticProps(context: any) {
-    const data =  await pageAPI.post(context.params.post)
-    return {
-        props: {
-            data
-        },
-        revalidate: 60,
-    };
+	const data = await pageAPI.post(context.params.post)
+	return {
+		props: {
+			data
+		},
+		revalidate: 60,
+	};
 }

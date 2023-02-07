0a76964afe445ccee83355f649661bbe2d9682e5
dev-fominov <dev-fominov@yandex.by>
2023-02-07 14:31:39 +0300

    add link canonical

--- services/Meta.tsx
+++ services/Meta.tsx
@@ -3,57 +3,65 @@ import Head from "next/head";
 import { useRouter } from "next/router";
 
 const DEFAULT_META = {
-    title: "",
-    description: "",
-    keywords: "",
-    robots_nofollow: "",
+	title: "",
+	description: "",
+	keywords: "",
+	robots_nofollow: "",
+	canonical: "",
 };
 
 interface MetaProps {
-    meta?: {
-        title?: string;
-        description?: string;
-        keywords?: string;
-        robots_nofollow?: string;
-    };
+	meta?: {
+		title?: string;
+		description?: string;
+		keywords?: string;
+		robots_nofollow?: string;
+		canonical?: string;
+	};
 }
 
 const Meta: FC<MetaProps> = ({ meta }) => {
-    const { title, description, keywords, robots_nofollow } = meta ?? DEFAULT_META;
-    const router = useRouter();
+	const { title, description, keywords, robots_nofollow, canonical } = meta ?? DEFAULT_META;
+	const router = useRouter();
 
-    return (
-        <Head>
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
-            <meta httpEquiv="X-DNS-Prefetch-Control" content="on" />
-            <meta httpEquiv="cleartype" content="on" />
-            <meta name="yandex-verification" content="0ed1b9903be3747d" />
-            <meta name="google-site-verification" content="_hxRc6xmVeOICnB6EIb8q405wLFT4cwuQghFJ6WovKM" />
-            <meta property="og:title" content={title ?? DEFAULT_META.title} />
-            <meta
-                property="og:description"
-                content={description ?? DEFAULT_META.description}
-            />
-            <meta property="og:locale" content="en_US" />
-            <meta property="og:image:width" content="968" />
-            <meta property="og:image:height" content="504" />
-            <meta property="og:url" content={router.pathname} />
-            <meta name='robots' content={`index, ${robots_nofollow}, max-image-preview:large, max-snippet:-1, max-video-preview:-1`} />
+	return (
+		<Head>
+			<meta name="viewport" content="width=device-width, initial-scale=1" />
+			<meta httpEquiv="X-UA-Compatible" content="IE=Edge" />
+			<meta httpEquiv="X-DNS-Prefetch-Control" content="on" />
+			<meta httpEquiv="cleartype" content="on" />
+			<meta name="yandex-verification" content="0ed1b9903be3747d" />
+			<meta name="google-site-verification" content="_hxRc6xmVeOICnB6EIb8q405wLFT4cwuQghFJ6WovKM" />
+			<meta property="og:title" content={title ?? DEFAULT_META.title} />
+			<meta
+				property="og:description"
+				content={description ?? DEFAULT_META.description}
+			/>
+			<meta property="og:locale" content="en_US" />
+			<meta property="og:image:width" content="968" />
+			<meta property="og:image:height" content="504" />
+			<meta property="og:url" content={router.pathname} />
+			{canonical == '10'
+				? <link rel="canonical" href="https://art-lichnost.ru/camp/professions" />
+				: canonical == '11'
+					? <link rel="canonical" href="https://art-lichnost.ru/camp/skills" />
+					: <link rel="canonical" href="https://art-lichnost.ru/camp/art-community" />}
 
-            <meta
-                name="keywords"
-                lang="ru"
-                content={keywords ?? DEFAULT_META.keywords}
-            />
-            <title>{title ?? DEFAULT_META.title}</title>
-            <meta
-                name="description"
-                content={description ?? DEFAULT_META.description}
-            />
-            
-        </Head>
-    );
+			<meta name='robots' content={`index, ${robots_nofollow}, max-image-preview:large, max-snippet:-1, max-video-preview:-1`} />
+
+			<meta
+				name="keywords"
+				lang="ru"
+				content={keywords ?? DEFAULT_META.keywords}
+			/>
+			<title>{title ?? DEFAULT_META.title}</title>
+			<meta
+				name="description"
+				content={description ?? DEFAULT_META.description}
+			/>
+
+		</Head>
+	);
 };
 
 export default Meta;
